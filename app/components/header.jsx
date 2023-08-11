bd4f7b2b14679e709685aab51b3554497cad80b5
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-08-11 17:43:38 +0100

    Added to styling of the home page

---
+++ app/components/header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="bg-slate-800 rounded-lg w-3/4 m-auto my-4 drop-shadow-md">
+      <p className="text-center text-white text-2xl font-bold p-2 font-[verdana]">
+        My FULL STACK Blog App with Next.js
+      </p>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,5 +1,43 @@
-export default function Home() {
+import Link from 'next/link';
+import Header from './components/header';
+
+async function getPosts() {
+  const posts = await fetch('http://localhost:3000/api/blog', {
+    next: { revalidate: 10 },
+  });
+  return posts.json();
+}
+
+export default async function Home() {
+  const { posts } = await getPosts();
   return (
-    <p className="text-red-500">HI</p>
-  )
+    <>
+      <Header />
+      <div className="flex justify-center">
+        <Link className="bg-slate-200 p-2 px-10 rounded" href="/blog/new">
+          Add new Blog 🚀
+        </Link>
+      </div>
+      <div className="flex flex-col gap-4 items-center">
+        {posts.map((post) => (
+          <div
+            key={post.id}
+            className="flex flex-col bg-slate-200 rounded p-4 w-3/4 mt-2"
+          >
+            <div className='flex justify-between'>
+              <h1>{post.title}</h1>
+              <Link
+                href={`/blog/edit/${post.id}`}
+                className="bg-slate-700 text-white rounded-md py-1 px-2"
+              >
+                Edit
+              </Link>
+            </div>
+            <h1 className='mb-2'>{new Date(post.date).toDateString()}</h1>
+            <h1>{post.description}</h1>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
